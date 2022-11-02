898d9c7c7dbeb854f6db913c40f8d22c5491387f
Joey <43940776+adoboligh1@users.noreply.github.com>
2022-11-02 16:30:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
 //include 'lofi.php';
 
-
-
 $azura = "http://exclusive.streamafrica.net/api/nowplaying/48?$rand";
+
+$url = "http://api.streamafrica.net/boxradio/web/nowplaying/lofi.php";
+
 
 $rand = rand(0, 999);
 
@@ -35,21 +36,28 @@ function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = tru
 
 $out = (getMp3StreamTitle('http://65.108.202.157:1000/radio.mp3', 8192));
 
-$url = "http://api.streamafrica.net/boxradio/web/nowplaying/lofi.php";
+
 
 $clean = str_replace(" ", "-", $out);
+
 // Deezer
 $query = "https://api.deezer.com/search?q=$clean";
 $file2 = file_get_contents($url);
 $file3 = file_get_contents($azura);
 $file3parsed = json_decode($file3);
-$next_album = $file3parsed->now_playing->song->album;
+$current_album = $file3parsed->now_playing->song->album;
 $next_genre = $file3parsed->now_playing->song->genre;
+$next_artist = $file3parsed->playing_next->song->artist;
 $next_artwork = $file3parsed->playing_next->song->art;
+$next_genre = $file3parsed->playing_next->song->genre;
+$current_genre = $file3parsed->now_playing->song->genre;
+
 $next_title = $file3parsed->playing_next->song->title;
+$next_album = $file3parsed->playing_next->song->album;
 $next_duration = $file3parsed->playing_next->duration;
 $file2parsed = json_decode($file2);
 $songss = $file2parsed->currentSong;
+$songhistory = $file2parsed->songHistory;
 $songs2 = $file2parsed->currentArtist;
 $file = file_get_contents($query);
 $parsedFile = json_decode($file);
@@ -58,32 +66,36 @@ $artist = $parsedFile->data[0]->artist->name;
 $title = $parsedFile->data[0]->title;
 $duration = $parsedFile->data[0]->duration;
 
-$nextsongdl = "box_lofi_next";
+//$nextsongdl = "box_lofi_next";
 
-$dir = dirname(__FILE__) . '/cover/' . $artist . '-' . $title . '-' . $radioname . '.jpg';
-$dir3 = dirname(__FILE__) . '/cover/' . $artist . '-' . $title . '-' . $nextsongdl . '.jpg';
+$pnghd = str_replace('jpg', 'png', $albumart);
+
+$dir = dirname(__FILE__) . '/cover/'  . $artist . '-' . $title . '-' . $radioname . '.jpg';
+$dir3 = dirname(__FILE__) . '/cover/' . $artist . '-' . $title . '-' . $radioname . '.png';
 $dir = str_replace(' ', '-', $dir);
 $dir = str_replace(',', '-', $dir);
-$dir2 = str_replace(' ', '-', $dir2);
-$dir2 = str_replace(',', '-', $dir2);
-$dir2 = dirname(__FILE__) . '/cover/' . $artist . '-' . $title . '-' . $radioname . '.png';
+$dir3 = str_replace(' ', '-', $dir2);
+$dir3 = str_replace(',', '-', $dir2);
+$dir3 = dirname(__FILE__) . '/cover/' . $artist . '-' . $title . '-' . $radioname . '.png';
 file_put_contents($dir, file_get_contents($albumart));
-file_put_contents($dir2, file_get_contents($albumart));
-file_put_contents($dir3, file_get_contents($next_artwork));
+file_put_contents($dir3, file_get_contents($pnghd));
 
 
-$yeet2 = 'https://covers.streamafrica.net/' . $artist . '-' . $title . '-' . $radioname . '.jpg';
+$yeet2 = 'https://covers.streamafrica.net/' . $artist . '-' . $title . '-' . $radioname . '.png';
 //$yeet2 = 'https://covers.streamafrica.net/' . $artist . '-' . $title . '-' . $nextsongdl . '.jpg';
-$yeet1 = 'https://covers.streamafrica.net/' . $artist . '-' . $title . '-' . $nextsongdl . '.jpg';
+$yeet1 = 'https://covers.streamafrica.net/' . $artist . '-' . $title . '-' . $radioname . '.jpg';
 $yeet2 = str_replace(' ', '-', $yeet2);
-$yeet2 = str_replace(',', '-', $yeet2);
 $yeet3 = 'https://covers.streamafrica.net/' . $artist . '-' . $title . '-' . $radioname . '.png';
 $yeet3 = str_replace(',', '-', $yeet3);
 $yeet3 = str_replace(' ', '-', $yeet3);
+$yeet1 = str_replace(',', '-', $yeet1);
+$yeet1 = str_replace(' ', '-', $yeet1);
 
 
-$array['nowplaying'] = ['artist' => $songs2, 'track' => $songss, 'artwork' => $yeet2, 'album' => $next_album, 'genre' => $next_genre, 'duration' => gmdate("i:s", $duration)];
-//$array['nextSong'] = ['artist' => $next_artist, 'track' => $next_song, 'artwork' => $yeet1, 'duration' => gmdate("i:s", $next_duration)];
+
+$array ['now_playing'] = ['artist' => $songs2, 'track' => $songss, 'artwork' => $yeet1, 'artwork_cdn' => $albumart, 'album' => $current_album, 'genre' => $current_genre, 'duration' => gmdate("i:s", $duration)];
+$array['next_song'] = ['artist' => $next_artist, 'track' => $next_title, 'artwork' => $next_artwork, 'album' => $next_album, 'genre' => $next_genre, 'duration' => gmdate("i:s", $next_duration)];
+$array ['history'] = $songhistory[0];
 //$array['artist'] = $songs2;
 //$array['song'] = $songss;
 //$array['artwork_jpg'] = $yeet2;
@@ -95,7 +107,7 @@ $urlHost = $_SERVER['HTTP_HOST'];
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
 echo (json_encode($array));
